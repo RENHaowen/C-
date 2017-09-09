425d452a170b825fffd043b116d14bc245f89c1b
RENHaowen <RENHaowen@users.noreply.github.com>
2017-09-10 00:23:39 +0200

    add folder 2.72

---
+++ 2.72/2.72/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  2.72
+//
+//  Created by RenHaowen on 2017/9/9.
+//  Copyright © 2017年 RenHaowen. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int distance;
+    cout << "Entrez une diatance qui est en base de long : ";
+    cin >> distance ;
+    int distance_c;
+    distance_c = distance * 200;
+    cout << distance << " long est " << distance_c << endl;
+}
