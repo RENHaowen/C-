cb125de8091cb62262aeb3eb9711362e39505748
RENHaowen <RENHaowen@users.noreply.github.com>
2017-09-10 00:22:32 +0200

    add file 2.71

---
+++ 2.71/2.71/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  2.71
+//
+//  Created by RenHaowen on 2017/9/9.
+//  Copyright © 2017年 RenHaowen. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "REN Haowen \n";
+    cout << "33 Rue Ernest Thierry Mieg, 90000, Belfort \n";
+}
