91a5ad3fff5078c17fd2988f27d55bdee2045ace
RENHaowen <RENHaowen@users.noreply.github.com>
2017-09-10 00:48:29 +0200

    add folder 2.74

---
+++ 2.74/2.74/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  2.74
+//
+//  Created by RenHaowen on 2017/9/9.
+//  Copyright © 2017年 RenHaowen. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    cout << "Entre ton age : ";
+    cin >> age;
+    int mois;
+    mois = age * 12;
+    cout << "Tu as deja pass " << mois << " mois. \n";
+    return 0;
+}
