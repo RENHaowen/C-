3ac88c73407b8483bb98a326ced83ec0c1791021
RENHaowen <RENHaowen@users.noreply.github.com>
2017-09-10 00:48:00 +0200

    add folder 2.73

---
+++ 2.73/2.73/main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  2.73
+//
+//  Created by RenHaowen on 2017/9/9.
+//  Copyright © 2017年 RenHaowen. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+void first_sentence()
+{
+    cout << "Three blind mice \n";
+}
+
+void second_sentence()
+{
+    cout << "See how they run\n";
+}
+
+int main()
+{
+    first_sentence();
+    first_sentence();
+    second_sentence();
+    second_sentence();
+}
+
